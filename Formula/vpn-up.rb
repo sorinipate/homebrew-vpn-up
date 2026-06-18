87747e892c8dbd3d4065d5076c5a4a4825c5ef77
Sorin-Doru Ipate <sorin.ipate@gmail.com>
2026-06-18 05:55:06 +0300

    vpn-up v3.9.0

--- Formula/vpn-up.rb
+++ Formula/vpn-up.rb
@@ -1,8 +1,8 @@
 class VpnUp < Formula
   desc "Secure command-line client for Cisco AnyConnect and other SSL VPNs"
   homepage "https://github.com/sorinipate/vpn-up-for-openconnect"
-  url "https://github.com/sorinipate/vpn-up-for-openconnect/archive/refs/tags/v3.8.0.tar.gz"
-  sha256 "899b562c1ae870e51939c81da6ffe4f945850508af35b90a80cbd3d80389296d"
+  url "https://github.com/sorinipate/vpn-up-for-openconnect/archive/refs/tags/v3.9.0.tar.gz"
+  sha256 "eec0dda9e8de622ceeef81431a1cb72950c4a232e051b21294437aaaed60da25"
   license "MIT"
 
   depends_on "bash"
